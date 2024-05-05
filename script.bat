23ee59495aec9aa6dd99e75ba30d05113aa8fce7
Dmitry Ponyatov <dponyatov@gmail.com>
2024-05-05 17:49:10 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- script.bat
+++ script.bat
@@ -11,14 +11,15 @@ md %MOUNT%
 
 dism /Mount-Wim /WimFile:%WIM% /index:1 /MountDir:%MOUNT%
 
-set DRIVER=%PE%\driver\%ARCH%
+set DRIVER=%PE%\driver
 md %DRIVER%
 
 rem %PE%/driver/%ARCH%/vbox
 rem D:\VBoxWindowsAdditions-x86.exe   /extract /D=%PE%\driver\x86
 rem D:\VBoxWindowsAdditions-amd64.exe /extract /D=%PE%\driver\amd64
 
-dism /Image:%MOUNT% /Add-Driver /Driver:%DRIVER% /recurse
+dism /Image:%MOUNT% /Add-Driver /Driver:%DRIVER%\%ARCH% /recurse
+dism /Image:%MOUNT% /Add-Driver /Driver:%DRIVER%\usb    /recurse
 
 set TOOL=%PE%\tool
 md %TOOL%
@@ -26,4 +27,6 @@ md %TOOL%
 xcopy /e /y %TOOL%   %MOUNT%\tool\
 xcopy /e /y %PE%\SDI %MOUNT%\SDI\
 
+dism /Unmount-Wim /MountDir:%MOUNT% /Commit
+rem /Discard
 
